deba4d8dc45bfd8140517bd30a5085c6be7b98dc
Rafael Figueiredo <rafael.figueiredo91@gmail.com>
2019-09-01 14:52:47 -0300

    postgres sample

---
+++ tdd/Hash/test_hash.go
@@ -0,0 +1,31 @@
+package hash
+
+
+import(
+	"fmt"
+	"testing"
+)
+
+
+func TestBCrypt(t *testing.T) {
+
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+/*
+func ExampleRepeat() {
+	str := Repeat("b", 5)
+	fmt.Print(str)
+	//Output: bbbbb
+}
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+*/
